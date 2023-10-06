4e7267dfa1ad043289a4df81ac8e0ec51c314008
mfahampshire <mfahampshire@protonmail.ch>
2023-10-06 14:25:52 +0200

    colourscheme change

--- config.h
+++ config.h
@@ -16,14 +16,14 @@ static unsigned int gappov    = 30;       /* vert outer gap between windows and 
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=16", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#222222";
+static int topbar             = 0;        /* 0 means bottom bar */
+static char *fonts[]          = { "monospace:size=11", "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true"  };
+static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#0000ff";
+static char selbgcolor[]            = "#000000";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
